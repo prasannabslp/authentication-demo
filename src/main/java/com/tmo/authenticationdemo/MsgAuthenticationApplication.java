c7438202aa15d67ad76f82f2217473bf8ae0c26d
Prasanna <prasannabslp@gmail.com>
2024-07-12 22:48:31 +0530

    pushing code to github

---
+++ src/main/java/com/tmo/authenticationdemo/MsgAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.tmo.authenticationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgAuthenticationApplication.class, args);
+	}
+
+}
